14f9caa3afc5493918cb5081310ee1672b0dddbc
AthiraRamesh <athirarameshm@gmail.com>
2023-10-24 17:28:55 +0530

    BlocBuilder( BlocBuilder will update the UI based on state update)

--- lib/todo/views/todo_page.dart
+++ lib/todo/views/todo_page.dart
@@ -3,8 +3,19 @@ import 'package:bloc_example/todo/cubit/todo_state.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-class TodoPage extends StatelessWidget {
+class TodoPage extends StatefulWidget {
   const TodoPage({super.key});
+
+  @override
+  State<TodoPage> createState() => _TodoPageState();
+}
+
+class _TodoPageState extends State<TodoPage> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,6 +25,11 @@ class TodoPage extends StatelessWidget {
       ),
       body: BlocBuilder<TodoCubit, TodoState>(
         builder: (context, state) {
+          if (state is InitTodoState) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
           return Text(state.toString());
         },
       ),
