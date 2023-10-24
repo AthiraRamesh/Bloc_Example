8508d3fe3ff39133b70f8589228783418a40035e
AthiraRamesh <athirarameshm@gmail.com>
2023-10-24 17:09:57 +0530

    Creating TodoState( Creating State for all possible state of the screen)

---
+++ lib/todo/cubit/todo_state.dart
@@ -0,0 +1,9 @@
+abstract class TodoState {}
+
+class InitTodoState extends TodoState {}
+
+class LoadingTodoState extends TodoState {}
+
+class ErrorTodoState extends TodoState {}
+
+class ResponseTodoState extends TodoState {}
